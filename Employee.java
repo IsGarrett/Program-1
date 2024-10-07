5ef7db6cea6bac5b1bc33a72fedf3ad0c22403ed
IsGarrett <166757512+IsGarrett@users.noreply.github.com>
2024-10-07 12:30:40 -0700

    Commit 2

--- Employee.java
+++ Employee.java
@@ -1,3 +1,43 @@
-public class Employee {
+public abstract class Employee {
+    
+    protected double empSalary;
+    protected double empCash;
+    protected String empName;
+
+
+    public Employee(double empSalary, double empCash, String empName) {
+
+        this.empSalary = empSalary;
+        this.empCash = empCash;
+        this.empName = empName;
+
+    }
+
+    public void getName(){
+
+        String getName = empName;
+
+    }
+
+    public void getSalary(){
+
+        double getSalary = empSalary;
+    }
+
+    public void getCash() {
+
+        double getCash = empCash;
+    }
+
+    public double getPaid() {
+
+        double getPaid = (empSalary / 26) + empCash; 
+
+        return getPaid;
+
+    }
+
+    public abstract double giveRaise(double raisePercent);
+
     
 }
--- SoftwareEngineer.java
+++ SoftwareEngineer.java
@@ -1,3 +1,34 @@
-public class SoftwareEngineer {
+public class SoftwareEngineer extends Employee {
     
+    
+    private String name;
+
+    public SoftwareEngineer(double empSalary, double empCash, String empName){
+
+        super(empSalary, empCash, empName);
+
+        empCash = 0; 
+    }
+
+    public double giveRaise(double raisePercent){
+
+        double newSalary = (empSalary / 26) * raisePercent;
+
+        return newSalary;
+
+    }
+
+    public interface Hourly {
+    
+        
+    }
+
+
+
+
+
+
+
+
+
 }
